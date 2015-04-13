4a6a5935c104cda5fd40bd030e244ed9d24f8557
Arthur Allshire <arthur.allshire@gmial.com>
2015-04-13 14:56:01 +1000

    Comment subsystems

--- src/Subsystems/Move/Chassis.h
+++ src/Subsystems/Move/Chassis.h
@@ -1,6 +1,24 @@
 #ifndef CHASSIS
 #define CHASSIS
 
+/* This is the chassis subsystem.
+ * Often considered the "mother of all subsystems" the chassis
+ * subsystem combines instructions from the driver (in teleop),
+ * the PID loop and field centered driving to make the robot move.
+ */
+
+/*
+ * A subsystem is an interface written by the programmer to control
+ * the systems of the robot.
+ * Subsystems are used by commands to provide a (hopefully)
+ * easy to use interface to the lower level functions of
+ * the robot and to keep all of the code directly controlling
+ * one system in a single place.
+ * Each subsystem monitors and controls a "system" on the robot
+ * (such as the chassis subsystem controlling the real-life drive
+ * system).
+ */
+
 #include "WPILib.h"
 
 class Chassis: public Subsystem
@@ -9,6 +27,7 @@ private:
 public:
     Chassis();
     ~Chassis();
+    void Drive(double vX, double vY, double vZ, double throttle);
 };
 
 #endif
--- src/Subsystems/PID/GyroCorrection.h
+++ src/Subsystems/PID/GyroCorrection.h
@@ -1,6 +1,12 @@
 #ifndef GYROCORRECTION
 #define GYROCORRECTION
 
+/* The GyroCorrection subsysem takes the output of
+ * the robot's yaw pid loop and translates it into
+ * a value that can be sent to the robot's motors
+ * to correct it's heading.
+ */
+
 #include "WPILib.h"
 
 class GyroCorrection: public PIDOutput
