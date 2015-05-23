9804b3914c8614b06ee427742268b7279cda07bb
Arthur Allshire <arthur.allshire@gmial.com>
2015-05-23 12:37:15 +1000

    PID constructor and PidOutput skeleton written

--- pid.py
+++ pid.py
@@ -1,4 +1,16 @@
 class Pid(object):
     """Class to run a PID control loop."""
+    def __init__(self, pid_output, kP, kI=0.0, kD=0.0, kF=0.0):
+        self.kP=kP
+        self.kI=kI
+        self.kD=kD
+        self.kF=kF
+        if not(isinstance(pid_output, PidOutput)):
+            raise Exception("Must pass in a PidOutput object")
+        return kP
+
+
+class PidOutput(object):
+    """Class that Pid objects output to."""
     def __init__(self):
         pass
--- test/test_pid.py
+++ test/test_pid.py
@@ -1,10 +1,10 @@
 """Unit tests for PID submodule."""
 
 import unittest
-from pid import Pid
+from pid import Pid, PidOutput
 
 class TestPid(unittest.TestCase):
 
-    def test_init(self):
-        p = Pid()
+    def test_init_return(self):
+        p = Pid(1, PidOutput())
         self.assertNotEqual(p, None)
