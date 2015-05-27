9db55cfd5b15bcc4001de420c053eceee6f3f3b6
James Ward <j.ward@acfr.usyd.edu.au>
2015-05-27 17:17:09 +1000

    Minor refactor of PWM class.

--- pwm.py
+++ pwm.py
@@ -1,17 +1,21 @@
+PWM1A = "/sys/devices/ocp.3/pwm_test_P9_14" # the directory where the files to set for pwm output 1a are held
+IDS = [PWM1A] # a list of all pwm ids for the pwm object to check against to ensure that we are passed a real directory
+DUTY = "/duty" # the file that holds the current duty cycle in ns
+STATUS = "/run" # the file that holds a 1 or a 0 telling the bbb weather to have pwm on or not
+PERIOD = "/period" # the file that holds the PWM period, in ns
+
+import glob
+
 class Pwm(object):
-    PWM1A = "/sys/devices/ocp.3/pwm_test_P9_14" # the directory where the files to set for pwm output 1a are held
-    IDS = [PWM1A] # a list of all pwm ids for the pwm object to check against to ensure that we are passed a real directory
-    DUTY = "/duty" # the file that holds the current duty cycle in ns
-    STATUS = "/run" # the file that holds a 1 or a 0 telling the bbb weather to have pwm on or not
-    FULL_BACK_DUTY = 600000 # nanoseconds
-    ZERO_DUTY = 1500000 #nanoseconds
-    FULL_DUTY = 2400000 #nanoseconds
-    ZERO_TO_FULL = (FULL_DUTY - FULL_BACK_DUTY)/2
         
-    def __init__(self, pwm_id):
-        if not pwm_id in self.IDS:
+    def __init__(self, pwm_id, min_duty = 600000, max_duty = 2400000, period = 10000000):
+        if not pwm_id in IDS:
             raise Exception("Must pass in a path to a BBB pwm file (see pwm.IDS)")
-        self.pwm_id = pwm_id
+        self.pwm_id = glob.glob(pwm_id + '.*')[0]
+        self.min_duty = min_duty
+        self.max_duty = max_duty
+        self.period = period
+        self.write((self.pwm_id+PERIOD), str(period))
         self.set_speed(0.0) # set our speed to 0 before turning pwm no
         self.pwm_on()
         
@@ -19,8 +23,8 @@ class Pwm(object):
         if not -1 <= speed <= 1:
             raise Exception("Speed must be passed as a float between -1 and 1")
         # convert speed from float from 0 to 1 into a time in ns
-        duty_in_ns = (speed * self.ZERO_TO_FULL) + self.ZERO_TO_FULL + self.FULL_BACK_DUTY
-        self.write(self.pwm_id+self.DUTY, str(duty_in_ns))
+        duty_in_ns = ((speed+1.0)/2.0) * (self.max_duty - self.min_duty) + self.min_duty
+        self.write(self.pwm_id + DUTY, str(duty_in_ns))
         return duty_in_ns
     
     def write(self, path, data):
@@ -30,7 +34,7 @@ class Pwm(object):
         f.close()
         
     def pwm_on(self):
-        self.write((self.pwm_id+self.STATUS), "1")
+        self.write((self.pwm_id+STATUS), "1")
     
     def pwm_off(self):
-        self.write((self.pwm_id+self.STATUS), "0")
+        self.write((self.pwm_id+STATUS), "0")
--- test/test_pwm.py
+++ test/test_pwm.py
@@ -1,6 +1,7 @@
 """Unit tests for PID submodule."""
 
 import unittest
+import pwm
 from pwm import Pwm
 import tempfile, os
 
@@ -8,25 +9,44 @@ class PwmTest(unittest.TestCase):
     
     def setUp(self):
         tmpdir = tempfile.mkdtemp()
-        os.chroot(tmpdir)
-        
-        if not os.path.exists(Pwm.PWM1A):
-            os.makedirs(Pwm.PWM1A)
+        # Create a temporary node tree
+        self.PWM99A = tmpdir + "/sys/devices/ocp.3/pwm_test_P9_99"
+        self.PWM99A_dir = self.PWM99A + '.12' # Device tree files have an incrementing .XX suffix
+        pwm.IDS.append(self.PWM99A)
+        if not os.path.exists(self.PWM99A_dir):
+            os.makedirs(self.PWM99A_dir)
+    
+    def test_pwm_init(self):
+        p = Pwm(self.PWM99A)
+        # Test default values
+        self.assertEqual(p.pwm_id, self.PWM99A_dir)
+        self.assertEqual(p.min_duty, 600000)
+        self.assertEqual(p.max_duty, 2400000)
+        self.assertEqual(p.period, 10000000)
+        self.assertEqual(10000000, float(open(self.PWM99A_dir +pwm.PERIOD).read()))
+        # Test non-default values
+        p = Pwm(self.PWM99A, 10000, 20000, 100000)
+        self.assertEqual(p.min_duty, 10000)
+        self.assertEqual(p.max_duty, 20000)
+        self.assertEqual(p.period, 100000)
+        self.assertEqual(100000, float(open(self.PWM99A_dir +pwm.PERIOD).read()))
     
     def test_pwm_output(self):
-                
-        pwm = Pwm(Pwm.PWM1A)
-        self.assertEqual(pwm.pwm_id, pwm.PWM1A)
+        min = 10000
+        max = 20000
+        zero = (min + max)/2.0
+        period = 100000
+        p = Pwm(self.PWM99A, min, max, period) # Use round numbers to make things easier
         
-        speed = pwm.set_speed(0)
-        self.assertEqual(speed, Pwm.ZERO_DUTY)
-        self.assertEqual(speed, float(open(Pwm.PWM1A+Pwm.DUTY).read()))
-        speed = pwm.set_speed(0.5)
-        self.assertEqual(speed, Pwm.ZERO_DUTY+(Pwm.ZERO_TO_FULL/2))
-        self.assertEqual(speed, float(open(Pwm.PWM1A+Pwm.DUTY).read()))
-        speed = pwm.set_speed(-1.0)
-        self.assertEqual(speed, Pwm.FULL_BACK_DUTY)
-        self.assertEqual(speed, float(open(Pwm.PWM1A+Pwm.DUTY).read()))
-        speed = pwm.set_speed(1.0)
-        self.assertEqual(speed, pwm.FULL_DUTY)
-        self.assertEqual(speed, float(open(Pwm.PWM1A+Pwm.DUTY).read()))
+        speed = p.set_speed(0)
+        self.assertEqual(speed, zero)
+        self.assertEqual(zero, float(open(self.PWM99A_dir+pwm.DUTY).read()))
+        speed = p.set_speed(0.5)
+        self.assertEqual(speed, (max+zero)/2.0)
+        self.assertEqual((max+zero)/2.0, float(open(self.PWM99A_dir+pwm.DUTY).read()))
+        speed = p.set_speed(-1.0)
+        self.assertEqual(speed, min)
+        self.assertEqual(min, float(open(self.PWM99A_dir+pwm.DUTY).read()))
+        speed = p.set_speed(1.0)
+        self.assertEqual(speed, max)
+        self.assertEqual(max, float(open(self.PWM99A_dir+pwm.DUTY).read()))
