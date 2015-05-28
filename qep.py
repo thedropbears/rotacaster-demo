4df830ab2fc59d8e52ec3d271ecda5fdc8d60afa
Arthur Allshire <arthur.allshire@gmial.com>
2015-05-29 07:48:26 +1000

    Start QEP class and fix some test things

---
+++ qep.py
@@ -0,0 +1,44 @@
+import glob
+
+class Qep(object):
+    PORTS = {
+        "QEP1" : "/sys/devices/ocp.2/48300000.epwmss/48300180.eqep",
+        "QEP2" : "/sys/devices/ocp.2/48302000.epwmss/48302180.eqep",
+        "QEP3" : "/sys/devices/ocp.2/48304000.epwmss/48304180.eqep" }
+    
+    # The counting mode of the encoder, 0=absolute, 1=relative
+    MODE = "/mode"
+    # The file for the period of the encoder -
+    # or how long it takes for the timer for velocity to reset, in ns
+    PERIOD = "/period"
+    # The position of the encoder. In absolute mode this represents the current position;
+    # in relative mode this represents its position at the last unit timer overflow
+    POSITION = "/position"
+    
+    MODE_ABSOLUTE = 0
+    MODE_RELATIVE = 1
+    
+    def __init__(self, qep_id, mode = MODE_ABSOLUTE, cpr = 180, period = 100000000, position = 0):
+        if not qep_id in Qep.PORTS.keys():
+            raise Exception("Must pass in a recognised BBB QEP port: " + str(Qep.PORTS.keys()))
+        self.qep_id = qep_id
+        #self.qep_dir = glob.glob(Qep.PORTS[qep_id])
+        self.qep_dir = Qep.PORTS[qep_id]
+        if mode == Qep.MODE_RELATIVE and period <= 0:
+            raise Exception("Must pass in a valid counting period greater than zero in relative mode")
+        self.mode = mode
+        self.cpr = cpr
+        self.period = period
+        # Write the period to a file
+        if self.mode == Qep.MODE_RELATIVE:
+            self.write(self.qep_dir+self.PERIOD, self.period)
+        self.position = 0
+        self.write(self.qep_dir+self.POSITION, self.position)
+            
+    def write(self, path, data):
+        """Overwrites or creates file at path and writes data to it"""
+        f = open(path, "w")
+        f.write(data)
+        f.close()
+        
+    
--- test/test_qep.py
+++ test/test_qep.py
@@ -10,11 +10,11 @@ class QepTest(unittest.TestCase):
     def setUp(self):
         tmpdir = tempfile.mkdtemp()
         # Create a temporary node tree
-        self.QEP99 = "/sys/devices/ocp.2/00000000.epwmss/00000099.eqep"
-        self.QEP99_dir = tmpdir + "/sys/devices/ocp.2/00000000.epwmss/00000099.eqep"
-        Qep.IDS["QEP99"] = self.QEP99
-        if not os.path.exists(self.PWM99A_dir):
-            os.makedirs(self.PWM99A_dir)
+        self.QEP99 = "/sys/devices/ocp.2/48306000.epwmss/48306180.eqep"
+        self.QEP99_dir = tmpdir + self.QEP99
+        Qep.PORTS["QEP99"] = self.QEP99
+        if not os.path.exists(self.QEP99_dir):
+            os.makedirs(self.QEP99_dir)
     
     def set_qep_count(self, position):
         # Open the mode attribute file
@@ -38,15 +38,15 @@ class QepTest(unittest.TestCase):
         # Ensure that qep is correctly turning the id into a file
         self.assertEqual(q.qep_dir, self.QEP99)
         # Ensure that the encoder is in relative mode, as commanded
-        self.assertEqual(Qep.MODE_RELATIVE, int(open(self.QEP99_dir+Qep.MODE).read()))
+        self.assertEqual(Qep.MODE_ABSOLUTE, int(open(self.QEP99_dir+Qep.MODE).read()))
         # Ensure that we are zeroing the encoder file value
         self.assertEqual(0.0, float(open(self.QEP99_dir+Qep.POSITION).read()))
         
         # Test non default values
         #          ID        Counting Mode    Counts per Rev.   Period   Position
-        q = Qep("QEP99", Qep.MODE_ABSOLUTE,     360,           100000000,   720)
+        q = Qep("QEP99", Qep.MODE_RELATIVE,     360,           100000000,   720)
         # Ensure that we are setting the right mode
-        self.assertEqual(Qep.MODE_ABSOLUTE, int(open(self.QEP99_dir+Qep.MODE).read()))
+        self.assertEqual(Qep.MODE_RELATIVE, int(open(self.QEP99_dir+Qep.MODE).read()))
         # Ensure that we are setting the correct counts per rev
         self.assertEqual(360, q.cpr)
         # Ensure that we are setting the period
