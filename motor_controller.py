c81ced2b597a3dfa26b30cd60a8c570748c66a3f
Arthur Allshire <arthur.allshire@gmail.com>
2015-06-07 11:11:54 +1000

    Clean up code

--- motor_controller.py
+++ motor_controller.py
@@ -1,24 +1,22 @@
 """A class that combines Pwm outputs with Qep based Pid for velocity control"""
 
-from pwm import Pwm
-from qep import Qep
-from pid import Pid, PidOutput
+import threading, time
 from pid import PidOutput
-import threading
-import time
+from qep import Qep
 
 class MotorController(threading.Thread):
     
     #list of motor controllers with pwm values in a dict inside the dict
     MOTOR_CONTROLLER_VALUES = {"VICTOR_SP" : {"min_duty" : 600000, "max_duty" : 2400000, "period" : 10000000}}
     
-    def __init__(self, pwm, pid, qep, pid_enabled = True):
+    def __init__(self, pwm, pid, pid_output, qep, pid_enabled = True):
         super(MotorController, self).__init__()
         if qep.mode == Qep.MODE_RELATIVE:
             raise Exception("Must pas in a Qep object in absolute mode")
         self.pwm = pwm
         self.pid = pid
         self.qep = qep
+        self.pid_output = pid_output
         self.pid_enabled = pid_enabled
         self.speed = 0
         if self.pid_enabled and self.pid.kF == 0:
@@ -27,24 +25,24 @@ class MotorController(threading.Thread):
         self.running.set()
         self.daemon = True
         self.last_time = time.time()
-        self.last_qep = qep.getRevolutions()
+        self.last_qep = qep.get_revolutions()
         self.start()
     
     def run(self):
-        print "tick"
         while self.running.isSet():
             # Get the number of revolutions relative to the start point 
-            print "tick"
-            self.qep_position = self.qep.getRevolutions()
+            self.qep_position = self.qep.get_revolutions()
             change_since_last = self.qep_position - self.last_qep # the change since the last iteration
             self.last_qep = self.qep_position
-            self.speed_after_pid = self.pid.update(change_since_last)
+            self.pid.update(change_since_last)
+            self.speed_after_pid = self.pid_output.value
             self.pwm.set_speed(self.speed_after_pid)
             time.sleep(0.02 - (time.time() - self.last_time))
             self.last_time = time.time()
     
-    def set_speed(self):
-        pass
+    def set_speed(self, speed):
+        assert(-1.0 <= speed <= 1.0, "Must pass in a speed in range -1 to 1")
+        self.pid.set_set_point(speed)
     
 class VelocityPidOutput(PidOutput):
     correction = 0
--- test/test_motor_controller.py
+++ test/test_motor_controller.py
@@ -1,27 +1,31 @@
 """Unit tests for the motor controller submodule"""
 
-import unittest
+import unittest, time
 from pid import PidOutput
 from motor_controller import MotorController
 from threading import Thread
 
 class MockPwm(object):
     def set_speed(self, speed):
-        pass
+        self.speed = speed
 
 class MockQep(object):
     def __init__(self):
         from qep import Qep
         self.mode = Qep.MODE_ABSOLUTE
-    def getRevolutions(self):
+    def get_revolutions(self):
         return 0
 
 class MockPid(object):
     def __init__(self, pid_output):
         self.pid_output = pid_output
         self.kF = 1.0
+        self.set_point = 0.0
     def update(self, value):
-        self.pid_output.set(value)
+        self.pid_output.set(self.set_point)
+    def set_set_point(self, set_point):
+        self.set_point = set_point
+        
 
 class MockPidOutput(PidOutput):
     def set(self, value):
@@ -32,12 +36,12 @@ class MotorControllerTest(unittest.TestCase):
     def setUp(self):
         p = MockPwm()
         
-        pid_output = MockPidOutput()
-        p_controller = MockPid(pid_output)
+        self.p_output = MockPidOutput()
+        p_controller = MockPid(self.p_output)
         
         q = MockQep()
         
-        self.m = MotorController(p, p_controller, q)
+        self.m = MotorController(p, p_controller, self.p_output, q)
         pass
     
     def test_motor_controller_init(self):
@@ -51,4 +55,11 @@ class MotorControllerTest(unittest.TestCase):
         self.assertEqual(self.m.pid_enabled, True)
         
     def test_motor_controlling(self):
-        pass        
+        
+        self.m.set_speed(1.0)
+        
+        self.assertEqual(1.0, self.m.pid.set_point)
+        
+        time.sleep(0.2)
+        
+        self.assertEqual(self.p_output.value, 1.0)
