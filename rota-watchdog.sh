86b36d793fb2007b19a39f385235e14641a9eaaa
James Ward <j.ward@acfr.usyd.edu.au>
2015-06-19 15:56:36 +1000

    Fixed watchdog to zero PWM values correctly.

--- rota-watchdog.sh
+++ rota-watchdog.sh
@@ -5,9 +5,9 @@ while true; do
     ps up `cat /var/run/rotacaster.pid ` >/dev/null
     if [ $? != 0 ]; then
       # Program has crashed
-      echo 0 > /sys/devices/ocp.3/pwm_test_P9_29/duty
-      echo 0 > /sys/devices/ocp.3/pwm_test_P8_36/duty
-      echo 0 > /sys/devices/ocp.3/pwm_test_P8_45/duty
+      echo 0 > /sys/devices/ocp.3/pwm_test_P9_29.*/duty
+      echo 0 > /sys/devices/ocp.3/pwm_test_P8_36.*/duty
+      echo 0 > /sys/devices/ocp.3/pwm_test_P8_45.*/duty
     fi
   fi
   sleep 0.5
